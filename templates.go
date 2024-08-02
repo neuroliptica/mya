78d65c9ab15e0ab8b2c26d2a6aeebd1035c969c5
itsuwari <102465369+neuroliptica@users.noreply.github.com>
2024-08-02 14:03:25 +0300

    templates

---
+++ templates.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"html/template"
+	"io"
+)
+
+// Combine compiled template with it's file representation.
+type Template struct {
+	Path     string
+	Compiled *template.Template
+}
+
+func (t *Template) Compile() error {
+	c, err := template.ParseFiles(t.Path)
+	if err != nil {
+		return err
+	}
+	t.Compiled = c
+
+	return nil
+}
+
+// Same as executing normal template.
+func (t *Template) Execute(wr io.Writer, data any) error {
+	if t.Compiled == nil {
+		return errors.New("template: nil pointer executed")
+	}
+	return t.Compiled.Execute(wr, data)
+}
+
+// Used as templates["template_name"].Execute(in, struct)
+var templates = map[string]*Template{
+	"main_page": {
+		Path: "templates/main_page.tmpl",
+	},
+}
+
+// Compile all templates at initialization.
+func CompileTemplates() error {
+	for k := range templates {
+		err := templates[k].Compile()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
