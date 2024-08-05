f07deb7ad2070de820730963dd9bea3f104e9de8
itsuwari <102465369+neuroliptica@users.noreply.github.com>
2024-08-05 18:12:07 +0300

    missed replies amount in omit node

--- html.go
+++ html.go
@@ -17,6 +17,9 @@ type MainView struct {
 type ThreadView struct {
 	OP      Post
 	Replies []Post
+
+	// Total replies amount minus last three.
+	Missed int64
 }
 
 type Paging struct {
@@ -58,26 +61,32 @@ func serveBoard(c echo.Context) error {
 	if err != nil || page < 0 {
 		page = 0
 	}
+
+	// Check if board exists.
 	board, err := checkRecord(&Board{Link: b})
 	if err != nil {
 		return c.String(http.StatusBadRequest, err.Error())
 	}
 
+	// Get all threads for current board.
 	var posts []Post
 	err = get(&posts, "board = ? AND parent = ?", board.Link, 0)
 	if err != nil {
 		return c.String(http.StatusBadRequest, err.Error())
 	}
+
 	// Sorting by last bump.
 	sort.Slice(posts, func(i, j int) bool {
 		return posts[i].LastBump.After(posts[j].LastBump)
 	})
+
 	// Get total pages on board.
 	pages := len(posts)/10 + 1
 	if len(posts)%10 == 0 && len(posts) != 0 {
 		pages--
 	}
-	// Getting posts with offset for current page.
+
+	// Get threads with offset for current page.
 	lb := page * 10
 	rb := (page + 1) * 10
 	rb = int(math.Min(float64(rb), float64(len(posts))))
@@ -87,6 +96,7 @@ func serveBoard(c echo.Context) error {
 	}
 	posts = posts[lb:rb]
 
+	// Get all boards for header.
 	// todo: cache to avoid requests to db.
 	var boards []Board
 	err = get(&boards)
@@ -110,9 +120,10 @@ func serveBoard(c echo.Context) error {
 	// Create view entry for every thread on board.
 	for i := range posts {
 		tv := ThreadView{
-			OP: posts[i],
+			OP:     posts[i],
+			Missed: 0,
 		}
-		// get replies
+		// Get last 3 replies.
 		res := db.Where(&Post{
 			Board:  board.Link,
 			Parent: posts[i].ID,
@@ -124,12 +135,24 @@ func serveBoard(c echo.Context) error {
 		if res.Error != nil {
 			return c.String(http.StatusInternalServerError, err.Error())
 		}
-
+		// Reverse the last 3 replies order.
 		sort.Slice(tv.Replies, func(i, j int) bool {
 			return tv.Replies[i].ID < tv.Replies[j].ID
 		})
+
+		// Get total amount of replies.
+		res = db.Model(&Post{}).
+			Where("board = ? AND parent = ?", board.Link, tv.OP.ID).
+			Count(&tv.Missed)
+
+		if res.Error != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		tv.Missed -= int64(len(tv.Replies))
 		bv.Threads = append(bv.Threads, tv)
 	}
+
 	// Render board page.
 	view := new(strings.Builder)
 	err = templates[BoardTemplate].Execute(view, bv)
